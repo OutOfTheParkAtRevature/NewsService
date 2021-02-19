d77c3b618c8461d3cbaa2ca843bda468cc68384c
Daniel Baker <dbaker731@gmail.com>
2021-02-19 14:44:07 -0600

    beginning of news controller and some edits to logic

---
+++ NewsService/Controllers/NewsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Service;
+
+namespace NewsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private readonly Logic _logic;
+
+        public NewsController(Logic logic)
+        {
+            _logic = logic;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<LeagueArticleDto>> GetLeagueArticles()
+        {
+            return await _logic.GetAllLeagueArticleDto();
+        }
+        [HttpGet]
+        public async Task<IEnumerable<LeagueArticleDto>> GetPinnedLeagueArticles()
+        {
+            return await _logic.GetPinnedLeagueArticleDto();
+        }
+        [HttpPost]
+        public async Task CreateLeagueArticle(LeagueArticleDto leagueArticleDto)
+        {
+            await _logic.CreateLeagueArticle(leagueArticleDto);
+        }
+        [HttpPut]
+        public async Task EditLeagueArticle(LeagueArticleDto leagueArticleDto)
+        {
+            await _logic.EditLeagueArticle(leagueArticleDto);
+        }
+        [HttpDelete("{id}")]
+        public async Task DeleteLeagueArticleById(Guid id)
+        {
+            await _logic.DeleteLeagueArticleById(id);
+        }
+        [HttpGet]
+        public async Task<IEnumerable<TeamArticleDto>> GetTeamArticles()
+        {
+            return await _logic.GetAllTeamArticleDto();
+        }
+        [HttpGet]
+        public async Task<IEnumerable<TeamArticleDto>> GetPinnedTeamArticles()
+        {
+            return await _logic.GetPinnedTeamArticleDto();
+        }
+        [HttpPost]
+        public async Task CreateTeamArticle(TeamArticleDto teamArticleDto)
+        {
+            await _logic.CreateTeamArticle(teamArticleDto);
+        }
+        [HttpPut]
+        public async Task EditTeamArticle(TeamArticleDto teamArticleDto)
+        {
+            await _logic.EditTeamArticle(teamArticleDto);
+        }
+        [HttpDelete("{id}")]
+        public async Task DeleteTeamArticle(Guid id)
+        {
+            await _logic.DeleteTeamArticleById(id);
+        }
+
+    }
+}
---
+++ Repository/Migrations/20210219202632_init.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LeagueArticles",
+                columns: table => new
+                {
+                    ArticleID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsVisible = table.Column<bool>(type: "bit", nullable: false),
+                    IsPinned = table.Column<bool>(type: "bit", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeagueArticles", x => x.ArticleID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TeamArticles",
+                columns: table => new
+                {
+                    ArticleID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TeamID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsVisible = table.Column<bool>(type: "bit", nullable: false),
+                    IsPinned = table.Column<bool>(type: "bit", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamArticles", x => x.ArticleID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LeagueArticles");
+
+            migrationBuilder.DropTable(
+                name: "TeamArticles");
+        }
+    }
+}
