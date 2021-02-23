89a876b7ecebe71ce31eff7afc82da0ecffd9c5a
Chris Sophiea <csophiea@gmail.com>
2021-02-23 13:37:45 -0700

    added httpclient to retrieve team info

--- NewsService/Controllers/NewsController.cs
+++ NewsService/Controllers/NewsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Model;
 using Service;
@@ -18,54 +19,56 @@ namespace NewsService.Controllers
             _logic = logic;
         }
 
-        [HttpGet]
+        [HttpGet("league")]
         public async Task<IEnumerable<LeagueArticleDto>> GetLeagueArticles()
         {
             return await _logic.GetAllLeagueArticleDto();
         }
-        [HttpGet]
+        [HttpGet("league/pinned")]
         public async Task<IEnumerable<LeagueArticleDto>> GetPinnedLeagueArticles()
         {
             return await _logic.GetPinnedLeagueArticleDto();
         }
-        [HttpPost]
+        [HttpPost("league")]
         public async Task CreateLeagueArticle(LeagueArticleDto leagueArticleDto)
         {
             await _logic.CreateLeagueArticle(leagueArticleDto);
         }
-        [HttpPut]
+        [HttpPut("league")]
         public async Task EditLeagueArticle(LeagueArticleDto leagueArticleDto)
         {
             await _logic.EditLeagueArticle(leagueArticleDto);
         }
-        [HttpDelete("{id}")]
+        [HttpDelete("league/{id}")]
         public async Task DeleteLeagueArticleById(Guid id)
         {
             await _logic.DeleteLeagueArticleById(id);
         }
-        [HttpGet]
+        [HttpGet("team")]
         public async Task<IEnumerable<TeamArticleDto>> GetTeamArticles()
         {
             // add logic to get team information
-            return await _logic.GetAllTeamArticleDto();
+            var token = await HttpContext.GetTokenAsync("access_token");
+            return await _logic.GetAllTeamArticleDto(token);
         }
-        [HttpGet]
+        [HttpGet("team/pinned")]
         public async Task<IEnumerable<TeamArticleDto>> GetPinnedTeamArticles()
         {
             // add logic to get team information
-            return await _logic.GetPinnedTeamArticleDto();
+            var token = await HttpContext.GetTokenAsync("access_token");
+            return await _logic.GetPinnedTeamArticleDto(token);
         }
-        [HttpPost]
+        [HttpPost("team")]
         public async Task CreateTeamArticle(TeamArticleDto teamArticleDto)
         {
             await _logic.CreateTeamArticle(teamArticleDto);
         }
-        [HttpPut]
+        [HttpPut("team")]
         public async Task EditTeamArticle(TeamArticleDto teamArticleDto)
         {
             await _logic.EditTeamArticle(teamArticleDto);
         }
-        [HttpDelete("{id}")]
+        [HttpDelete("team/{id}")]
         public async Task DeleteTeamArticle(Guid id)
         {
             await _logic.DeleteTeamArticleById(id);
